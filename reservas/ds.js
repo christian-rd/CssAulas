ed042507cade2f5e2ce72af7f0de2eb1d8f67c9a
christian-rd <christiandavi12345@gmail.com>
2022-10-06 18:41:09 +0000

    First Commit

---
+++ reservas/ds.js
@@ -0,0 +1,46 @@
+/*
+
+<table cellspacing="5" cellpadding="10" border="2" class="tabela">
+        <thead>
+          <tr>
+            <th>Estado</th>
+            <th>Nome</th>
+            <th>Área</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th>Amazonas</th>
+            <td>Acari </td>
+            <td> 896436m² </td>
+          </tr>
+          <tr>
+            <th rowspan="2">Bahia</th>
+            <td>Alto Cariri </td>
+            <td>19 238,02m² </td>
+          </tr>
+          <tr>
+            <td> Boa Nova</td>
+            <td>12 065,31m² </td>
+          </tr>
+          <tr>
+            <th rowspan="2">Maranhão</th>
+            <td>Anavilhanas </td>
+            <td>5896m² </td>
+          </tr>
+          <tr>
+            <th>Cabo Orange</th>
+            <td>18952m² </td>
+          </tr>
+
+        </tbody>
+        <tfoot>
+          <tr>
+            <th>Área toal</th>
+            <td colspan="2" align="center">15895490m²</td>
+          </tr>
+        </tfoot>
+
+      </table>
+
+*/
